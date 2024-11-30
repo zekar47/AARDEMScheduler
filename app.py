87a1d5b634973360dce4e052fe6e86162c9b9fc0
zekar47 <zekarmt47@proton.me>
2024-11-29 20:36:18 -0600

    v1.0.0

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2024 ZEKAR
+#
+# This file is part of AARDEMScheduler.
+#
+# AARDEMScheduler is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AARDEMScheduler is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with AARDEMScheduler. If not, see <https://www.gnu.org/licenses/>.
+
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from dataManager import loadData, saveData, FILENAME
 from scheduleManager import addTeacher, addClassType, addOverride, setClassStatus, getTeachers, getStudentNameForSlot
